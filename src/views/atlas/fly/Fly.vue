305134ed10bfb2faedc50f8bc01c6dcefcb09ad1
wynne <wynnewu@live.cn>
2023-11-16 15:55:20 +0800

    fly

--- src/views/atlas/fly/Fly.vue
+++ src/views/atlas/fly/Fly.vue
@@ -26,5 +26,33 @@ const neuroglancerDatas = [{
   options: {
     shader: "\nvoid main() {\n  setColor(defaultColor());\n  setEndpointMarkerColor(\n    vec3( 1.0, 0.0, 0.0),\n    vec3(0.0, 1.0, 0.0));\n  setEndpointMarkerSize(7.0);\n}\n"
   }
+}, {
+  name: "bbox",
+  label: "Bbox",
+  type: "segmentation",
+}, {
+  name: "cleft",
+  label: "Cleft",
+  type: "segmentation",
+}, {
+  name: "seg",
+  label: "Seg",
+  type: "segmentation",
+}, {
+  name: "synapse_ovals",
+  label: "Synapse Ovals",
+  type: "annotation",
+  options: {
+    shader: "\nvoid main() {\n  setColor(vec4(1.0, 0.0, 1.0, 0.6));\n  }\n"
+  }
+}, {
+  name: "inference",
+  label: "Inference",
+  type: "annotation",
+}, {
+  name: "inference_strip",
+  label: "Inference Strip",
+  type: "annotation",
+
 }]
 </script>
