636ba645f5e4c7464143c7c81f4266a791e8bbdd
hamjohn <804351197@qq.com>
2024-12-24 17:36:55 +0800

    fixed: first commit

--- src/main.js
+++ src/main.js
@@ -8,7 +8,7 @@ import * as ElementPlusIconsVue from "@element-plus/icons-vue";
 import 'element-plus/theme-chalk/display.css';
 import './assets/css/main.scss';
 import './assets/css/dark.scss';
-// import Keycloak from 'keycloak-js'
+import Keycloak from 'keycloak-js'
 
 const pinia = createPinia();
 const app = createApp(App);
@@ -17,28 +17,28 @@ for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
   app.component(key, component);
 }
 
-// const keycloak = new Keycloak({
-//   url: 'http://10.15.56.125:8080',
-//   realm: 'Test',
-//   grantType: 'confidential',
-//   accessType: 'public',
-//   clientId: 'dataplatform',
-//   // clientSecret: '7mWu8qjE8Ywcd2dUCOZfk348JAkINeBf',
-// })
+const keycloak = new Keycloak({
+  url: 'http://10.15.56.125:8080',
+  realm: 'Test',
+  grantType: 'confidential',
+  accessType: 'public',
+  clientId: 'dataplatform',
+  // clientSecret: '7mWu8qjE8Ywcd2dUCOZfk348JAkINeBf',
+})
 
-// try {
-//   const authenticated = await keycloak.init({
-//     onLoad: 'login-required',
-//   })
-//   console.log('authenticated---->', authenticated, keycloak)
-//   if (authenticated) {
-//     console.log('User is authenticated')
-//   } else {
-//     console.log('User is not authenticated')
-//   }
-// } catch (error) {
-//   console.error('Failed to initialize adapter:', error)
-// }
+try {
+  const authenticated = await keycloak.init({
+    onLoad: 'login-required',
+  })
+  console.log('authenticated---->', authenticated, keycloak)
+  if (authenticated) {
+    console.log('User is authenticated')
+  } else {
+    console.log('User is not authenticated')
+  }
+} catch (error) {
+  console.error('Failed to initialize adapter:', error)
+}
 
 app.use(pinia);
 app.use(i18n);
